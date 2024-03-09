a9eee1095a18da991a8e6a1fcd5f591223f9f1dc
Dolphin Mulugeta <dolphinmulugeta88@gmail.com>
2024-03-09 09:09:23 +0000

    Updated first commit

--- Arrays/creating.cpp
+++ Arrays/creating.cpp
@@ -3,7 +3,6 @@
 
 int main() {
     const int SIZE = 6;
-    
     // datatype variable_name with size
     int intArray[SIZE] = {3,4,5,6,7,8};
     std::string stringArray[SIZE] = {"Hello", "World","This","Is","Me","C++"};
@@ -11,8 +10,10 @@ int main() {
     for (int i = 0 ;i < 6;i++){
         std::cout << stringArray[i] << " ";
     }
+    std::cout << std::endl<<std::endl;
 
     int matrix[3][4] = {{1,2,3,4},{5,6,7,8},{9,0,1,2}};
+    
     for (int i = 0;i < 3;i++){
         for (int j = 0; j < 4 ; j++){
             std::cout << matrix[i][j]<<" ";
